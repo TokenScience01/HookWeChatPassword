5c088b7b84b46056c7b979cde7758a6811d5749a
jionecc <995000812@qq.com>
2018-06-09 00:10:03 +0800

    Hook微信账号密码

---
+++ app/src/main/java/com/jionego/HookDemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.jionego.HookDemo;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/hook/HookMain.java
@@ -0,0 +1,70 @@
+package hook;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import java.util.logging.Logger;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * = █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * <p>
+ * ---------------------------Created by Xyx on 2018/5/12.----------------
+ */
+
+public class HookMain implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpp) throws Throwable {
+
+        if (lpp.packageName.contains("com.tencent.mm")){
+            XposedBridge.log("进入微信");
+
+            XposedHelpers.findAndHookConstructor("com.tencent.mm.modelsimple.v",
+                    lpp.classLoader,
+                    String.class,
+                    String.class,
+                    String.class,
+                    int.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                            Object arg = param.args[0];
+                            Object arg1 = param.args[1];
+                            Object arg2 = param.args[2];
+                            Object arg3 = param.args[3];
+
+                            XposedBridge.log("arg:"+arg+"\narg1:"+arg1+"\narg2:"+arg2+"\n"+"arg3:"+arg3);
+                        }
+                    }
+            );
+        }
+
+    }
+
+
+
+
+}
+
+
